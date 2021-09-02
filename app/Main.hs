67f19af646ea360783f5d61632f0427617f5a098
skalnark <skalnark@live.com>
2021-09-02 16:45:56 -0300

    functions moved to src/PDI.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,11 @@ module Main
   where
 
 import Data.Either ()
-import Codec.Picture.Types(Pixel)
+import PDI
 import Codec.Picture
-    ( convertRGB8,
-      readImage,
-      savePngImage,
-      pixelMap,
-      Image,
-      DynamicImage(ImageRGB8),
-      Pixel8,
-      PixelRGB8(..))
-
-data YIQ = YIQ Double Double Double
+          ( savePngImage,
+            convertRGB8,
+            readImage)
 
 main :: IO ()
 main = do
@@ -35,51 +28,3 @@ main = do
 
   print "done"
 
-imageMap :: Pixel a =>
-  (a -> PixelRGB8) -> Image a -> DynamicImage
-imageMap f img = ImageRGB8 $ pixelMap f img
-
-redComponent :: PixelRGB8 -> PixelRGB8
-redComponent (PixelRGB8 r g b) = PixelRGB8 r 0 0
-
-greenComponent :: PixelRGB8 -> PixelRGB8
-greenComponent (PixelRGB8 r g b) = PixelRGB8 0 g 0
-
-blueComponent :: PixelRGB8 -> PixelRGB8
-blueComponent (PixelRGB8 r g b) = PixelRGB8 0 0 b
-
-changeBrightness :: Double -> PixelRGB8 -> PixelRGB8
-changeBrightness bright img = yiq $ rgbToYiq img 
-  where
-    yiq (YIQ y i q) = yiqToRgb $ YIQ (y + bright) i q 
-
-backAndAgain :: PixelRGB8 -> PixelRGB8
-backAndAgain = yiqToRgb . rgbToYiq 
-
-trunc :: Pixel8 -> Pixel8
-trunc x
-  | x <= 0 = 0
-  | x >= 255 = 255
-  | otherwise = x
-
-negativeFromRGB :: PixelRGB8 -> PixelRGB8 
-negativeFromRGB (PixelRGB8 r g b) = PixelRGB8 (-r) (-g) (-b)
-
-negativeFromY :: PixelRGB8 -> PixelRGB8 
-negativeFromY img = negative $ rgbToYiq img 
-  where
-    negative (YIQ y i q) = yiqToRgb $ YIQ (-y) i q
-
-yiqToRgb :: YIQ -> PixelRGB8
-yiqToRgb (YIQ y i q) = PixelRGB8 r g b
-  where
-    r = trunc $ round $ y + (i * 0.956) + (q * 0.621)
-    g = trunc $ round $ y - (i * 0.272) - (q * 0.647)
-    b = trunc $ round $ y - (i * 1.106) + (q * 1.703)
-
-rgbToYiq :: PixelRGB8 -> YIQ
-rgbToYiq (PixelRGB8 r g b) = YIQ y i q
-  where
-    y = fromIntegral r * 0.299 + fromIntegral g * 0.587 + fromIntegral b * 0.114
-    i = fromIntegral r * 0.596 - fromIntegral g * 0.274 - fromIntegral b * 0.322
-    q = fromIntegral r * 0.211 - fromIntegral g * 0.523 + fromIntegral b * 0.312
